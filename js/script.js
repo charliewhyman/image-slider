1c861247a3a5d564eff675fd0b80aa83c30236c9
cwhyman <cwhyma@gmail.com>
2022-02-21 10:49:28 -0500

    Added functions for showing next/previous image function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,51 @@
-"use strict";
+'use strict';
+
+const image = document.getElementById('image');
+const backArrow = document.getElementById('backArrow');
+const forwardsArrow = document.getElementById('forwardsArrow');
+
+//create an array to store the path to the images to be displayed
+const imageArray = [
+  'images/image1.jpg',
+  'images/image2.jpg',
+  'images/image3.jpg',
+];
+
+//create an index to keep track of which image to display
+let currentImage = 0;
+
+//create a function to change the image source to the next one in the array
+const nextSlide = function nextSlide() {
+  //progress the slideshow by one
+  if (currentImage < imageArray.length - 1) {
+    currentImage += 1;
+    image.src = imageArray[currentImage];
+    //once the last image is reached, reset the index to 0 to display the first image
+  } else {
+    currentImage = 0;
+    image.src = imageArray[currentImage];
+  }
+};
+
+//add an event listener to the forwards arrow
+forwardsArrow.addEventListener('click', () => {
+  nextSlide();
+});
+
+//create a function to change the image source to the previous one in the array
+const previousSlide = function previousSlide() {
+  if (currentImage > 0) {
+    //decrease the slideshow by one
+    currentImage -= 1;
+    image.src = imageArray[currentImage];
+    //once the first image is reached, reset the index to (array length - 1) to display the last image
+  } else {
+    currentImage = imageArray.length - 1;
+    image.src = imageArray[currentImage];
+  }
+};
+
+//add an event listener to the back arrow
+backArrow.addEventListener('click', () => {
+  previousSlide();
+});
